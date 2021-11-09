d5807adfd1aa15dd200535b4d4942399dee0601a
Gibson Han <gibsonxhan@gmail.com>
2021-11-09 16:14:25 +0700

    implement dark mode using next-theme library

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <main className="flex-grow flex flex-col justify-center items-center bg-gray-200">
+    <main className="flex-grow flex flex-col justify-center items-center dark:bg-black">
       <div className="text-2xl sm:text-6xl md:text-9xl">hello World</div>
     </main>
   );
