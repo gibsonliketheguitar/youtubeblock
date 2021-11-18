8e0125f1bd6d9ee233b07534b0d3afcefa908b0b
Gibson Han <gibsonxhan@gmail.com>
2021-11-18 18:26:15 +0700

    add next-auth with google provider

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,11 +1,22 @@
 import type { NextPage } from "next";
+import { useSession, signIn, signOut } from "next-auth/react"
 
 const Home: NextPage = () => {
-  return (
+  const { data: session } = useSession()
+  if (session) {
+    return <>
+      Signed in as {session.user?.email} <br />
+      <button onClick={() => signOut()}>Sign out</button>
+    </>
+  }
+  return <>
     <main className="flex-grow flex flex-col justify-center items-center dark:bg-black">
       <div className="text-2xl sm:text-6xl md:text-9xl">hello World</div>
+
+      Not signed in <br />
     </main>
-  );
+    <button onClick={() => signIn()}>Sign in</button>
+  </>
 };
 
 export default Home;
