ea513c9a30a972ba776eafdc8b6f6ed9eaf7d08b
Gibson Han <gibsonthedev@gmail.com>
2021-11-18 18:26:15 +0700

    add next-auth with google provider

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -2,14 +2,17 @@ import 'tailwindcss/tailwind.css'
 import type { AppProps } from 'next/app'
 import AppLayout from '@layout/AppLayout'
 import { ThemeProvider } from 'next-themes'
+import { SessionProvider } from "next-auth/react"
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <ThemeProvider defaultTheme='light' attribute='class'>
-      <AppLayout>
-        <Component {...pageProps} />
-      </AppLayout>
-    </ThemeProvider>
+    <SessionProvider session={session}>
+      <ThemeProvider defaultTheme='light' attribute='class'>
+        <AppLayout>
+          <Component {...pageProps} />
+        </AppLayout>
+      </ThemeProvider>
+    </SessionProvider>
   )
 }
 
