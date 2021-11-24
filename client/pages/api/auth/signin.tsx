a60f7a0fc9cbba966a349fbb626f84a6070ea8a1
Gibson Han <gibsonthedev@gmail.com>
2021-11-24 16:29:00 +0700

    refactor auth api. use try,catch, finally to reduce logic tree branching

--- client/pages/api/auth/signin.tsx
+++ client/pages/api/auth/signin.tsx
@@ -1,22 +1,33 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { getToken } from "next-auth/jwt";
 import { getSession } from "next-auth/react";
 
 const baseURL = 'http://localhost:3001'
+const secret = process.env.JWT_SECRET!
+
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+    const token = await getToken({ req, secret })
+    console.log('what is token', token, secret)
     const session = await getSession({ req })
-    if (session) {
+
+    try {
+        if (!session) throw 'Error'
+
         let response = await fetch(baseURL + '/auth/signin', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify(session.user)
+            body: JSON.stringify(session?.user)
         })
-        let result = await response.json()
-        if (result?.error) res.send({ message: result.message, error: result.error })
-        else res.send({ message: 'login successful' })
+
+        if (response.ok) res.status(200).send({ message: 'Login successful' })
+        else throw 'Error'
     }
-    else {
-        res.send({ message: 'Please check your credentials', error: 'Unauthorized' })
+    catch (error) {
+        res.status(401).send({ message: 'Please check your credentials', error: 'Unauthorized' })
+    }
+    finally {
+        res.end()
     }
 }
