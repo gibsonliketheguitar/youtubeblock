b1fca80522c1019fc963f24236289d5df1b0a0e2
gibsonliketheguitar <66423127+gibsonliketheguitar@users.noreply.github.com>
2021-12-04 15:57:44 +0700

    fix duplicate name bug

--- client/components/PrimeTimeList.tsx
+++ client/components/PrimeTimeList.tsx
@@ -1,9 +1,9 @@
 import Carousel from '@components/common/Carousel'
 import { default as PreviewBlock } from '@components/PrimeTimePreviewBlock'
-import PrimeTimeList from '@ts/interface/primeTimeList'
+import { default as PtInterface } from '@ts/interface/primeTimeList'
 import isArrEmpty from '@utils/isArrEmpty'
 
-export default function PrimeTimeList({ data }: PrimeTimeList) {
+export default function PrimeTimeList({ data }: PtInterface) {
     if (isArrEmpty(data)) return <></>
     return (
         <div className="flex flex-col">
